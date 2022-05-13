cdfd559b2cec555df9d9bc8dc321d15a4f229213
shimi uhlman <shimiuhlman@gmail.com>
2022-05-13 12:34:40 -0400

    adding a kmm module

---
+++ app/kmmEmployeeList/src/commonMain/kotlin/com/shimi/kmmemployeelist/Greeting.kt
@@ -0,0 +1,7 @@
+package com.shimi.kmmemployeelist
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ app/kmmEmployeeList/src/commonMain/kotlin/com/shimi/kmmemployeelist/Platform.kt
@@ -0,0 +1,5 @@
+package com.shimi.kmmemployeelist
+
+expect class Platform() {
+    val platform: String
+}
